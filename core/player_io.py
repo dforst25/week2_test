df45c93368360399a0ff7bbd5ffb6ed432905e32
דוד פורסט <dforst@g.jct.ac.il>
2025-10-30 11:56:30 +0200

    adding the ERROR output in the io file

--- core/player_io.py
+++ core/player_io.py
@@ -3,7 +3,7 @@ def is_valid_input(inp: str) -> bool:
 
 
 def ask_player_action() -> str:
-    choice = ''
+    choice = input("Enter your choice: 'H' or 'S'\nH = HIT, continue drawing\nS = STAND, stopping: ").upper()
     while not is_valid_input(choice):
-        choice = input("Enter your choice: 'H' or 'S'\nH = HIT, continue drawing\nS = STAND, stopping: ").upper()
+        choice = input("ERROR!!! Invalid Input\nEnter your choice: 'H' or 'S'\nH = HIT, continue drawing\nS = STAND, stopping: ").upper()
     return choice
